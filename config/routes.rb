d4501fb5531bb3cd67154421286a5d31cc08219a
Nathan Jacoby <natejacobymusic@gmail.com>
2019-12-19 21:50:11 +1000

    Add name to user forms

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  devise_scope :user do
+    get 'users/sign_up', to: 'users/registrations#new'
+    get 'users/sign_in', to: 'users/sessions#new'
+  end
   devise_for :users
 
   root to: 'dashboard#index'
